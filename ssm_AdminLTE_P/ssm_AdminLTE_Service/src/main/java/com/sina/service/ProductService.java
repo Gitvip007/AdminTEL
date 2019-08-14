2f0cffc7d1da48c5f326e225d1d70f4617f3ab69
git <1015027907@qq.com>
2019-08-14 18:28:49 +0800

    ssm测试

--- ssm_AdminLTE_P/ssm_AdminLTE_Service/src/main/java/com/sina/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.sina.service;
-
-import com.sina.domain.Product;
-
-import java.util.List;
-
-public interface ProductService {
-    public List<Product> findAll();
-}
--- ssm_AdminLTE_P/ssm_AdminLTE_dao/src/main/java/com/sina/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.sina.dao;
-
-import com.sina.domain.Product;
-
-import java.util.List;
-
-public interface ProductDao {
-    public List<Product> findAll();
-}
